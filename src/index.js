e6bd81f5a3db86c0a44e5d1fe8c4d239195c9c22
Some Nick <somenick@example.com>
2023-02-17 16:18:57 +0300

    feat: add task solution towel-sort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    if (arguments.length == 0) {
+        return [];
+    }
+    if (matrix.length === 0) {
+        return (matrix = []);
+    }
+    for (let i = 0; i < matrix.length; i = i + 2) {
+        matrix[i].sort((a, b) => a - b);
+    }
+    for (let j = 1; j < matrix.length; j = j + 2) {
+        matrix[j].sort((a, b) => b - a);
+    }
+    return matrix.flat();
+};
